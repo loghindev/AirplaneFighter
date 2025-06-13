bd7f0e697aec0c6c3d2fc9968f691dcfc6ea8931
Alexandru Loghin <loghin.dev@gmail.com>
2025-06-13 15:53:29 +0300

    save

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 //* ----- GAME FUNCTIONALITY ------
 //* Sections
 const mainMenu = document.getElementById("menu");
-const storeSection = document.getElementById("store");
 const gameSection = document.getElementById("game");
 const endMenu = document.getElementById("endMenu");
 const pauseMenu = document.getElementById("pauseMenu");
 //* Sections Buttons
 const playBtn = document.getElementById("playBtn");
-const storeBtn = document.getElementById("storeBtn");
-const backToMenuBtn = document.getElementById("backToMenu");
 const endMenuBtn = document.getElementById("endMenuBtn");
 const endRestartBtn = document.getElementById("endRestartBtn");
 const pauseMenuBtn = document.getElementById("pauseMenuBtn");
 const pauseContinueBtn = document.getElementById("pauseContinueBtn");
 
-const menuAirplane = document.getElementById("menuAirplane");
+//* Airplane
+export const menuAirplane = document.getElementById("menuAirplane");
 const airplane = document.getElementById("airplane");
 //* Garbage Collector
 const bulletsCollector = document.querySelector("#garbageCollector .bullets");
@@ -57,12 +55,17 @@ function loadGame() {
     setAirplaneBehavior();
     once = false;
   }
-  setTimeout(() => {
-    generateAsteroids();
-    generatePowerUpGifts();
-    generateRocketGifts();
-  }, 2000);
+  setTimeout(generateAsteroids, 0); //TODO - while dev
+  // const powerDelay = Math.floor(Math.random() * 3000) + 6000;
+  // const rocketDelay = Math.floor(Math.random() * 3000) + 6000;
+  // console.log(powerDelay);
+  // console.log(rocketDelay);
+
+  setTimeout(generatePowerUpGifts, 0); // powerDelay
+  setTimeout(generateRocketGifts, 0); // rocketDelay
+  indexScore = 0;
   handleScore();
+  document.body.style.cursor = "none";
 }
 function setHud() {
   hudHearts.textContent = "3";
@@ -83,26 +86,27 @@ function setAirplaneBehavior() {
 }
 function setAirplaneMovement() {
   document.addEventListener("mousemove", (event) => {
-    if (lockMovement || gameOver) return; // lock the movement while exploding effect
+    if (lockMovement || gameOver) return; // lockMovement is true while explosion effect
     const x = event.clientX;
     const y = event.clientY;
     airplane.style.transform = `translate(calc(${x}px - 50%), calc(${y}px - 50%))`;
   });
 }
 function setAirplaneShooting() {
+  //* Handle basic bullets (left click)
   document.addEventListener("click", (event) => {
     if (event.target.id !== "endRestartBtn") {
       shoot(event.clientX, event.clientY);
     }
   });
+  //* Handle rockets (right click)
   document.addEventListener("contextmenu", (event) => {
     event.preventDefault();
-    console.log(Number(hudRockets.textContent));
-
-    if (Number(hudRockets.textContent) > 0) {
+    if (Number(hudRockets.textContent) > 0 && !paused && !lockMovement && !gameOver) {
       shootRocket(event.clientX, event.clientY);
     } else {
       console.warn("NO BULLETS");
+      // run error sound
     }
   });
 }
@@ -120,31 +124,31 @@ function shootRocket(x, y) {
 
 //? Generate Asteroids
 function generateAsteroids() {
-  const delay = Math.floor(Math.random() * 4000);
-  const speedY = Math.floor(Math.random() * 3) + 2;
+  const delay = Math.floor(Math.random() * 3500);
+  const speedY = Math.floor(Math.random() * 3.5) + 2;
   const asteroid = createAsteroid();
   const randomX = Math.random() * innerWidth;
-  const y = -20;
+  const y = -100;
   moveAsteroid(asteroid, randomX, y, speedY);
   generateAsteroidId = setTimeout(generateAsteroids, delay);
 }
 
 //? Generate PowerUp Gifts
 function generatePowerUpGifts() {
-  const delay = Math.floor(Math.random() * 30000) + 30000;
+  const delay = Math.floor(Math.random() * 3000) + 3000;
   const powerUp = createPowerUpGift();
   const randomX = Math.random() * innerWidth;
-  const y = -20;
+  const y = -100;
   moveGift(powerUp, randomX, y);
   generatePowerUpGiftId = setTimeout(generatePowerUpGifts, delay);
 }
 
 //? Generate Rocket Gifts
 function generateRocketGifts() {
-  const delay = Math.floor(Math.random() * 125000) + 30000;
+  const delay = Math.floor(Math.random() * 3000) + 3000;
   const rocketGift = createRocketGift();
   const randomX = Math.random() * innerWidth;
-  const y = -20;
+  const y = -100;
   moveGift(rocketGift, randomX, y);
   generateRocketGiftId = setTimeout(generateRocketGifts, delay);
 }
@@ -153,7 +157,7 @@ function generateRocketGifts() {
 function generateCoinGifts(asteroidPointer) {
   const coinGift = createCoinGift();
   const rect = asteroidPointer.getBoundingClientRect();
-  moveGift(coinGift, rect.x, rect.y);
+  moveGift(coinGift, rect.x + rect.width / 2 - 18, rect.y);
   spinCoin(coinGift);
 }
 
@@ -161,6 +165,7 @@ function generateCoinGifts(asteroidPointer) {
 function createBullet() {
   const bullet = document.createElement("img");
   bullet.classList = "bullet";
+  //TODO Modifica bullet in functie de powerup (fix aici)
   bullet.src = "./assets/projectiles/red/slim.png";
   bulletsCollector.appendChild(bullet);
   return bullet;
@@ -168,8 +173,10 @@ function createBullet() {
 function createAsteroid() {
   const asteroid = document.createElement("img");
   asteroid.classList = "asteroid";
-  asteroid.src = "https://placehold.co/40x40?text=A";
-  asteroid.style.borderRadius = "12px";
+  //TODO Random Asteroid image (fix aici)
+  asteroid.src = asteroidsGallery[Math.floor(Math.random() * asteroidsGallery.length)];
+  asteroid.style.transform = "translate(-50%, -50%)";
+  asteroid.style.borderRadius = "50px";
   asteroidsCollector.appendChild(asteroid);
   return asteroid;
 }
@@ -190,6 +197,7 @@ function createRocketGift() {
 function createCoinGift() {
   const coinGift = document.createElement("img");
   coinGift.classList.add("coin-gift");
+  coinGift.style.width = "35px";
   coinsCollector.appendChild(coinGift);
   return coinGift;
 }
@@ -211,8 +219,9 @@ function moveBullet(bullet, x, y) {
   }
   bullet.style.transform = `translate(calc(${x}px - 50%), calc(${y}px - 50%))`;
   handleBulletsCollision(bullet);
+  //* Handle rocket movement and stop position
   if (bullet.classList.contains("rocket")) {
-    if (y > 150) {
+    if (y > -150) {
       bulletId = requestAnimationFrame(() => {
         moveBullet(bullet, x, y);
       });
@@ -223,8 +232,8 @@ function moveBullet(bullet, x, y) {
     }
     return;
   }
-
-  if (y > 100) {
+  //* Handle basic bullet movement and stop position
+  if (y > -150) {
     bulletId = requestAnimationFrame(() => {
       moveBullet(bullet, x, y);
     });
@@ -237,9 +246,9 @@ function moveBullet(bullet, x, y) {
 function moveAsteroid(asteroid, x, y, speedY) {
   let moveAsteroidId = null;
   y += speedY;
-  asteroid.style.transform = `translate(calc(${x}px - 50%), calc(${y}px - 50%))`;
+  asteroid.style.transform = `translate(${x}px, ${y}px)`;
   checkCollision(airplane, asteroid);
-  if (y < innerHeight - 100) {
+  if (y < innerHeight + 150) {
     moveAsteroidId = requestAnimationFrame(() => {
       moveAsteroid(asteroid, x, y, speedY);
     });
@@ -255,7 +264,7 @@ function moveGift(ability, x, y) {
   ability.style.transform = `translate(${x}px, ${y}px)`;
   checkCollision(airplane, ability);
   // sa dispara de tot din viewport - 100
-  if (y < innerHeight - 100) {
+  if (y < innerHeight + 150) {
     giftId = requestAnimationFrame(() => {
       moveGift(ability, x, y);
     });
@@ -274,26 +283,30 @@ function checkCollision(obj1, obj2) {
   if (a.top < b.bottom && a.bottom > b.top && a.right > b.left && a.left < b.right) {
     // found collision
     if (obj1.id === "airplane" && obj2.classList.contains("asteroid")) {
+      //!AIRPLANE X ASTEROID
       obj2.remove();
       destroyAirplane();
       decreasePower();
       decreaseHeart();
-      indexScore -= 1000;
-
+      indexScore -= 250;
       updateScoreValue();
     } else if (obj1.classList.contains("bullet") && obj2.classList.contains("asteroid")) {
+      //!BULLET X ASTEROID
       [1, 2, 3, 4][Math.floor(Math.random() * 3)] === 1 ? generateCoinGifts(obj2) : "";
       obj1.remove();
       explodeAsteroid(obj2);
-      incrementScore();
+      incrementScore(obj2);
     } else if (obj1.id === "airplane" && obj2.classList.contains("power-gift")) {
+      //!AIRPLANE X POWER UP GIFT
       obj2.remove();
       incrementPower();
     } else if (obj1.id === "airplane" && obj2.classList.contains("rocket-gift")) {
+      //!AIRPLANE X ROCKET GIFT
       obj2.remove();
       incrementRocketStorage();
       // console.log("airplane x rocket");
     } else if (obj1.id === "airplane" && obj2.classList.contains("coin-gift")) {
+      //!AIRPLANE X COIN GIFT
       obj2.remove();
       incrementCoins();
       // console.log("airplane x coin");
@@ -318,8 +331,10 @@ function incrementCoins() {
   let amount = Number(hudCoins.textContent);
   hudCoins.textContent = (++amount).toString();
 }
-function incrementScore() {
-  indexScore += 50;
+function incrementScore(obj) {
+  if (obj.classList.contains("asteroid")) {
+    indexScore += 50;
+  }
   updateScoreValue();
 }
 
@@ -347,6 +362,7 @@ function decreaseRocket() {
 //* --- DESTROY ---
 function destroyAirplane() {
   if (runningEffect) return; // don't run effect again
+  //* Set flags
   runningEffect = true;
   lockMovement = true;
   explodeAirplane(); // run effect
@@ -362,13 +378,14 @@ function explodeAirplane() {
       clearInterval(explodeAirplaneId);
       // console.log("explodeAirplaneId oprit");
       setTimeout(() => {
-        //* reset flags
+        //* Reset flags
         runningEffect = false;
         lockMovement = false;
         if (Number(hudHearts.textContent) <= 0) {
           endGame();
           return;
         }
+        //? WAAT THE HECK IS THIS
         airplane.style.transform = `translate(${innerWidth}px, ${innerHeight * 2}px)`;
         setAirplaneModel();
       }, 700);
@@ -378,16 +395,12 @@ function explodeAirplane() {
 function spinCoin(coin) {
   let index = 0;
   let spinCoinId = setInterval(() => {
-    if (index < coinSpin.length) {
-      // console.log("Spinning Coin...");
-      coin.src = coinSpin[index++];
-    } else {
+    if (index > coinSpin.length - 1) {
       index = 0;
-      coin.src = coinSpin[index++];
     }
+    coin.src = coinSpin[index++];
     if (coin.getBoundingClientRect().y === 0) {
       clearInterval(spinCoinId);
-      // console.log("spinCoinId() Stopped");
     }
   }, 50);
 }
@@ -397,7 +410,7 @@ function explodeAsteroid(asteroid) {
   let index = 0;
   const img = document.createElement("img");
   img.classList = "destroyed";
-  img.style.transform = `translate(${rect.x}px, ${rect.y + 20}px)`;
+  img.style.transform = `translate(calc(${rect.x + rect.width / 2}px - 50%), ${rect.y + 25}px)`;
   asteroidsCollector.appendChild(img);
   let explodeAsteroidId = setInterval(() => {
     if (index < asteroidExplosion.length) {
@@ -416,9 +429,10 @@ function endGame() {
   clearTimeout(generatePowerUpGiftId);
   clearTimeout(generateRocketGiftId);
   clearInterval(reloadInterval);
+  clearInterval(scoreId);
   displayEndContextMenu();
   clearScreen(null);
-  indexScore = 0;
+  // indexScore = 0;
   document.body.style.cursor = "default";
 }
 
@@ -428,7 +442,7 @@ function handleMainMenuContext() {
   playBtn.addEventListener("click", () => {
     mainMenu.classList.add("hidden-section");
     gameSection.classList.remove("hidden-section");
-    document.body.style.cursor = "none";
+
     gameOver = false;
     setTimeout(loadGame, 2);
   });
@@ -444,6 +458,7 @@ function handleEndMenuContext() {
   // Restart Game because you're addicted
   endRestartBtn.addEventListener("click", () => {
     endMenu.classList.add("hidden-section");
+
     gameOver = false;
     loadGame();
   });
@@ -477,11 +492,9 @@ function clearScreen(bullet) {
 
 function handleScore() {
   scoreId = setInterval(() => {
-    if (gameOver) clearInterval(scoreId);
-    console.log("updating Score");
-
     updateScoreValue();
-  }, 600);
+    // console.log("updating Score");
+  }, 666);
 }
 
 function updateScoreValue() {
@@ -530,7 +543,7 @@ function reloadEnergy() {
   reloadInterval = setInterval(() => {
     const bars = document.querySelectorAll("#energyPanel .energyBars .fill-percent");
     bars.forEach((bar) => {
-      const style = getComputedStyle(bar);
+      const style = window.getComputedStyle(bar);
       const translateX = Number(style.transform.split(", ")[4]);
       let newTranslateX = translateX - (!expired ? 1 : 2);
       if (newTranslateX >= -bar.getBoundingClientRect().width - 2) {
@@ -544,7 +557,7 @@ function reloadEnergy() {
   }, 20);
 }
 
-//* --- FRAMES PATHS ---
+//* --- PATHS ---
 const shipExplosion = [
   "./assets/explosion/ship/1.png",
   "./assets/explosion/ship/2.png",
@@ -571,7 +584,6 @@ const shipExplosion = [
   "./assets/explosion/ship/23.png",
   "./assets/explosion/ship/24.png",
 ];
-
 const coinSpin = [
   "./assets/coin/spr_coin_0.png",
   "./assets/coin/spr_coin_1.png",
@@ -605,7 +617,6 @@ const coinSpin = [
   "./assets/coin/spr_coin_29.png",
   "./assets/coin/spr_coin_30.png",
 ];
-
 const asteroidExplosion = [
   "./assets/explosion/asteroid/1.png",
   "./assets/explosion/asteroid/2.png",
@@ -616,4 +627,14 @@ const asteroidExplosion = [
   "./assets/explosion/asteroid/7.png",
   "./assets/explosion/asteroid/8.png",
   "./assets/explosion/asteroid/9.png",
+];
+const asteroidsGallery = [
+  "./assets/asteroids/big/big1.png",
+  "./assets/asteroids/big/big2.png",
+  "./assets/asteroids/big/big3.png",
+  "./assets/asteroids/big/big4.png",
+  "./assets/asteroids/medium/medium1.png",
+  "./assets/asteroids/medium/medium2.png",
+  "./assets/asteroids/small/small1.png",
+  "./assets/asteroids/small/small2.png",
 ];
---
+++ scripts.js/inventory.js
@@ -0,0 +1,122 @@
+import { menuAirplane } from "../script.js";
+
+const shipsImagesWrapper = document.querySelector("#inventory .inventory-ships .images");
+const bgImagesWrapper = document.querySelector("#inventory .inventory-backgrounds .images");
+const bgImage = document.getElementById("bgImage");
+const SPEED = 0.6;
+let firstSession = true;
+
+//* --- PATHS ---
+const shipsGallery = [
+  "../assets/ships/type1/1.png",
+  "../assets/ships/type1/2.png",
+  "../assets/ships/type1/3.png",
+  "../assets/ships/type1/4.png",
+  "../assets/ships/type2/1.png",
+  "../assets/ships/type2/2.png",
+  "../assets/ships/type2/3.png",
+  "../assets/ships/type2/4.png",
+  "../assets/ships/type3/1.png",
+  "../assets/ships/type3/2.png",
+  "../assets/ships/type3/3.png",
+  "../assets/ships/type3/4.png",
+];
+
+const bgGallery = [
+  "../assets/backgrounds/1.jpeg",
+  "../assets/backgrounds/2.jpeg",
+  "../assets/backgrounds/3.jpeg",
+  "../assets/backgrounds/4.jpeg",
+];
+
+document.addEventListener("DOMContentLoaded", loadInventory);
+
+function loadInventory() {
+  loadShips();
+  loadBackgrounds();
+}
+
+function loadShips() {
+  const length = shipsGallery.length;
+  for (let i = 0; i < length; ++i) {
+    const ship = document.createElement("img");
+    ship.src = shipsGallery[i];
+    ship.style.width = "100%";
+    ship.style.filter = "drop-shadow(0 0 20px white)";
+    shipsImagesWrapper.appendChild(ship);
+  }
+  //* HANDLE BUTTONS
+  let index = 0;
+  const ships = document.querySelectorAll("#inventory .inventory-ships .images img");
+  const shipsPrevBtn = document.querySelector("#inventory .inventory-ships .gallery .prev");
+  const shipsNextBtn = document.querySelector("#inventory .inventory-ships .gallery .next");
+  shipsPrevBtn.addEventListener("click", () => {
+    ships[index--].style.display = "none";
+    if (index < 0) index = length - 1;
+    ships[index].style.display = "block";
+    const path = shipsGallery[index].slice(1);
+    menuAirplane.src = path;
+  });
+  shipsNextBtn.addEventListener("click", () => {
+    ships[index++].style.display = "none";
+    if (index === length) index = 0;
+    ships[index].style.display = "block";
+    const path = shipsGallery[index].slice(1);
+    menuAirplane.src = path;
+  });
+}
+
+function loadBackgrounds() {
+  const length = bgGallery.length;
+  for (let i = 0; i < length; ++i) {
+    const bg = document.createElement("img");
+    bg.src = bgGallery[i];
+    bg.style.width = "100%";
+    bgImagesWrapper.appendChild(bg);
+  }
+  //* HANDLE BUTTONS
+  let index = 0;
+  const backgrounds = document.querySelectorAll("#inventory .inventory-backgrounds .images img");
+  const bgPrevBtn = document.querySelector("#inventory .inventory-backgrounds .gallery .prev");
+  const bgNextBtn = document.querySelector("#inventory .inventory-backgrounds .gallery .next");
+  bgPrevBtn.addEventListener("click", () => {
+    backgrounds[index--].style.display = "none";
+    if (index < 0) index = length - 1;
+    backgrounds[index].style.display = "block";
+    const path = bgGallery[index].slice(1);
+    bgImage.src = path;
+    firstSession = false;
+  });
+  bgNextBtn.addEventListener("click", () => {
+    backgrounds[index++].style.display = "none";
+    if (index === length) index = 0;
+    backgrounds[index].style.display = "block";
+    const path = bgGallery[index].slice(1);
+    bgImage.src = path;
+    firstSession = false;
+  });
+}
+
+//* --- BACKGROUND SCROLL ---
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+bgImage.src = bgGallery[0].slice(1);
+let bgY1 = 0;
+let bgY2 = -canvas.height;
+
+bgImage.onload = function () {
+  if (!firstSession) return;
+  function gameLoop() {
+    bgY1 += SPEED;
+    bgY2 += SPEED;
+    if (bgY1 >= canvas.height) bgY1 = bgY2 - canvas.height;
+    if (bgY2 >= canvas.height) bgY2 = bgY1 - canvas.height;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(bgImage, 0, bgY1, canvas.width, canvas.height);
+    ctx.drawImage(bgImage, 0, bgY2, canvas.width, canvas.height);
+    requestAnimationFrame(gameLoop);
+  }
+  gameLoop();
+};
